563f2466e0f0718a4c688312cea5657baa020884
choihana <hana9213@naver.com>
2021-10-29 22:31:52 +0900

    coummunity_hana

--- app/src/main/java/com/example/dog/Community.java
+++ app/src/main/java/com/example/dog/Community.java
@@ -2,38 +2,31 @@ package com.example.dog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Community extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
+
+    Button write_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
-        /*getSupportFragmentManager().beginTransaction().
-                replace(R.id.main_layout,fragment1).commitAllowingStateLoss();*/
 
-        /*BottomNavigationView bottimNavigationView = findViewById(R.id.bottom);
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.tab1:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.main_layout, fragment1).commit();
-                        return true;
-
-                    case R.id.tab2:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.main_layout, fragment2).commit();
-                        return true;
-                    }
-
-                    return false;
-                }
-            });*/
+        write_btn=findViewById(R.id.btnwrite);
+        write_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 화면간 이동 데이터 전달
+                Intent intent = new Intent(getApplicationContext(), Writecommunity.class);
+                startActivity(intent);
+            }
+        });
         }
     }
---
+++ app/src/main/java/com/example/dog/Writecommunity.java
@@ -0,0 +1,29 @@
+package com.example.dog;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Writecommunity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.communtiy_write);
+
+
+
+
+    }
+}
