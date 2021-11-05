81b59c0e339cd3899ac030af5dfee363592f087d
choihana <hana9213@naver.com>
2021-11-05 20:38:08 +0900

    coummunity_hana

--- app/src/main/java/com/example/dog/Community.java
+++ app/src/main/java/com/example/dog/Community.java
@@ -3,15 +3,37 @@ package com.example.dog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Community extends AppCompatActivity {
 
-    Button write_btn,map_btn,community_btn,option_btn;
+    final private String TAG = getClass().getSimpleName();
+
+    Button write_btn, map_btn, community_btn, option_btn;
+    ListView listView;
+    String userid = "";
+
+    // 리스트뷰에 사용할 제목 배열
+    ArrayList<String> titleList = new ArrayList<>();
+
+    // 클릭했을 때 어떤 게시물을 클릭했는지 게시물 번호를 담기 위한 배열
+    ArrayList<String> seqList = new ArrayList<>();
 
 
     @Override
@@ -19,7 +41,38 @@ public class Community extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
 
-        map_btn=findViewById(R.id.mapmenu);
+        // 로그인에서 넘긴 userid값 받기..?
+        userid = getIntent().getStringExtra("userID");
+
+        listView = findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // 어떤 값을 선택했는지 토스트를 뿌려줌
+                Toast.makeText(Community.this, adapterView.getItemAtPosition(i) + " 클릭", Toast.LENGTH_SHORT).show();
+/*
+                // 게시물 크게 보기
+                // 게시물의 번호와 userid를 가지고 DetailActivity 로 이동
+                Intent intent = new Intent(Community.this, Detailcommunity.class);
+                intent.putExtra("board_seq", seqList.get(i));
+                intent.putExtra("userid", userid);
+                startActivity(intent);
+*/
+
+            }
+        });
+        write_btn = findViewById(R.id.btnWrite);
+        write_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // 화면간 이동 데이터 전달
+                Intent intent = new Intent(getApplicationContext(), Writecommunity.class);
+                intent.putExtra("userID",userid);
+                startActivity(intent);
+            }
+        });
+        //하단바 클릭으로 맵으로 이동
+        map_btn = findViewById(R.id.mapmenu);
         map_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,7 +80,7 @@ public class Community extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        //하단바 클릭으로 옵션으로 이동
         option_btn = findViewById(R.id.option);
         option_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,18 +89,57 @@ public class Community extends AppCompatActivity {
             }
         });
 
+    }
 
-        write_btn=findViewById(R.id.btnWrite);
-        write_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 화면간 이동 데이터 전달
-                Intent intent = new Intent(getApplicationContext(), Writecommunity.class);
-                startActivity(intent);
+    protected void onResume() {
+        super.onResume();
+        GetBoard getBoard = new GetBoard();
+        getBoard.execute();
+    }
+
+    class GetBoard extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            Log.d(TAG,"onPreExcute");
+        }
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG,"onPostExecute," + result);
+
+            titleList.clear();
+            seqList.clear();
+            try {
+                // 결과물이 JSONArray 형태로 넘어오기 때문에 파싱
+                JSONArray jsonArray = new JSONArray(result);
+
+                for(int i=0;i<jsonArray.length();i++){
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String title = jsonObject.optString("title");
+                    String seq = jsonObject.optString("seq");
+
+                    // title, seq 값을 변수로 받아서 배열에 추가
+                    titleList.add(title);
+                    seqList.add(seq);
+                }
+                // ListView 에서 사용할 arrayAdapter를 생성하고, ListView 와 연결
+                ArrayAdapter arrayAdapter = new ArrayAdapter<String>(Community.this, android.R.layout.simple_list_item_1, titleList);
+                listView.setAdapter(arrayAdapter);
+
+                // arrayAdapter의 데이터가 변경되었을때 새로고침
+                arrayAdapter.notifyDataSetChanged();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        });
+
         }
 
-
-
+        protected String doInBackground(String... strings) {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            return null;
+        }
     }
+}
--- app/src/main/java/com/example/dog/Writecommunity.java
+++ app/src/main/java/com/example/dog/Writecommunity.java
@@ -1,24 +1,48 @@
 package com.example.dog;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 
 public class Writecommunity extends AppCompatActivity {
 
+    final private String TAG = getClass().getSimpleName();
+
     private final int GET_GALLERY_IMAGE = 200;
     private ImageView imageview;
+    EditText content;
+    Button btnSave;
+    Bitmap uploadBitmap;
+
+    String userid = ""; //유저 아이디
+
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.communtiy_write);
+
+        userid = getIntent().getStringExtra("userID");
 
         imageview = (ImageView) findViewById(R.id.imageView);
         imageview.setOnClickListener(new View.OnClickListener() {
@@ -29,7 +53,145 @@ public class Writecommunity extends AppCompatActivity {
                 // 사진을 글쓰는 창에 올리기까지 가능. 글자취소선 이유 모르겠음...
             }
         });
+        //글쓰기?
+        /*content = (EditText) findViewById(R.id.content_et);
+        String contents = content.getText().toString();
+        contents = contents.replace("'","''");*/
+
+        content = (EditText) findViewById(R.id.content_et);
+
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //게시물 등록 함수
+                RegBoard regBoard = new RegBoard();
+                regBoard.execute(userid, content.getText().toString());
+
+                // 버튼클릭 했을 때 사진이랑 텍스트 같이 저장하기.
+                // 저장 성공하면 문구띄우고 글쓰기창 나가기
+                // 우선순위가 저장소 호출,,,
+
+
+
+            }
+        });
     }
+
+
+
+    class RegBoard extends AsyncTask<String,Void,String> {
+        protected void onPreExcute() {
+            super.onPreExecute();
+
+            Log.d(TAG, "onPreExcute");
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG,"onPostExecute" + result);
+
+            if(result.equals("success")) {
+                // 결과값이 successd이면 토스트 메시지 뿌리고 이전 액티비티로 이동.
+                // 이때 activity_community 의 onResume() 함수가 호출되면서, 데이터를 새로고침
+                Toast.makeText(Writecommunity.this,"등록되었습니다.", Toast.LENGTH_SHORT).show();
+                finish();
+            } else
+            {
+                Toast.makeText(Writecommunity.this,result,Toast.LENGTH_SHORT).show();
+            }
+        }
+        @Override
+        protected String doInBackground(String... params) {
+
+        }
+        // hph url 만들어지면 시도해보기.
+            /*String userid = params[0];
+            String content = params[1];
+
+            String server_url = "";
+
+
+            URL url;
+            String response = "";
+            try {
+                url = new URL(server_url);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("userid", userid)
+                        .appendQueryParameter("content", content);
+                String query = builder.build().getEncodedQuery();
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+
+                conn.connect();
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line=br.readLine()) != null) {
+                        response+=line;
+                    }
+                }
+                else {
+                    response="";
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }*/
+
+
+        // 아마 사진 저장하는 거....?
+        /*String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp";
+            String mi = System.currentTimeMillis() + ".png";
+            String realpath = dirPath + "/" + mi;
+
+            String str = Environment.getExternalStorageState();
+            if(str.equals(Environment.MEDIA_MOUNTED)) {
+                OutputStream outputStream = null;
+                File file = new File(dirPath);
+
+                if(!file.exists()) {
+                    file.mkdir();
+                }
+                try {
+                    outputStream = new FileOutputStream(dirPath + "/"+ mi);
+
+
+                    uploadBitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream);
+                    outputStream.flush();
+                    outputStream.close();
+                }
+                catch (Exception e) {
+
+                }
+            } else {
+                Toast.makeText(Writecommunity.this,"실패",Toast.LENGTH_SHORT).show();
+            }
+            return null;
+        }*/
+
+    }
+
+
 
     @Override
     // 갤러리에 있는 사진을 글쓰는 창에 올리는 코드
@@ -42,5 +204,6 @@ public class Writecommunity extends AppCompatActivity {
         }
     }
     // 우선은 저장해서 커뮤니티 메인에서 볼 수 있게 하기. 그 후에 서버연결 같이하기.
+
 
 }
