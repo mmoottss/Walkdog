16f81e1bd798cddefcf02a60366ac9146162a001
sohuipark <ck1808@naver.com>
2021-11-14 18:21:34 +0900

    option_sh

--- app/src/main/java/com/example/dog/option.java
+++ app/src/main/java/com/example/dog/option.java
@@ -1,58 +1,27 @@
 package com.example.dog;
 
-import android.content.Intent;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
-import androidx.preference.PreferenceScreen;
-
-//https://android-uni.tistory.com/7 2021.3 그나마 최신버전 Preference 사용법
-//https://recipes4dev.tistory.com/58 프래그먼트 사용법(위에서 참고했다고 함)
-//preference - https://developer.android.com/guide/topics/ui/settings/organize-your-settings?hl=ko
-
-//https://developer.android.com/guide/topics/ui/settings/organize-your-settings?hl=ko#preferencescreens
-// ㄴ> screen 중첩 계층 구조 선언 불가하다는 내용
-
-//https://developer.android.com/guide/topics/ui/settings/customize-your-settings?hl=ko
-// ㄴ>환경설정 작업 :: fragment 생성 후 인텐트 설정해보기
-
-//https://developer.android.com/training/basics/fragments/creating?hl=ko
-// ㄴ>프래그먼트 만드는 법
-//https://devatom.tistory.com/4
-// ㄴ>중첩 프래그먼트...
 
 public class option extends PreferenceFragmentCompat {
     SharedPreferences prefs;
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.option, rootKey);
+        setPreferencesFromResource(R.xml.main_option, rootKey);
 //        Intent intent = new Intent(getContext(), fragment.class);
 
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         prefs.registerOnSharedPreferenceChangeListener(prefListener);
     }
 
-    private Preference.OnPreferenceChangeListener onPreferenceChangeListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceTreeClick(Preference preference, Context context) {
 
-            return false;
-        }
-    };
-
-//    public boolean onPreferenceTreeClick(Preference preference) {
-//        return super.onPreferenceTreeClick(preference);
-//    }
-
-    public boolean onPreferenceStartScreen (PreferenceFragmentCompat caller, PreferenceScreen pref){
-        caller.setPreferenceScreen(pref);
         return true;
     }
 
@@ -64,6 +33,7 @@ public class option extends PreferenceFragmentCompat {
                 getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
             }
                 //야간모드 설정
+                //앱 재부팅시 야간모드 살아있어야함(수정 필요)
             if (prefs.getBoolean("night_mode", true)) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
             } else {
